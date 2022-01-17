02dcdabfc60f9c0b3f7605cb8f88730f0c9534bc
Daniel Schmitz <dschmitz01@web.de>
2022-01-17 18:00:03 +0100

    Static file serving with Opine

---
+++ server.ts
@@ -0,0 +1,27 @@
+import { opine, serveStatic } from "https://deno.land/x/opine@2.1.1/mod.ts";
+import { dirname, join } from "https://deno.land/x/opine@2.1.1/deps.ts";
+
+
+const env = Deno.env.get("DENO_ENV");
+const app = opine();
+
+const __dir = dirname(import.meta.url);
+
+
+app.use(serveStatic(join(__dir, "public")));
+app.use(serveStatic(join(__dir, "public", "css")));
+
+app.use("/static", serveStatic(join(__dir, "public")));
+
+/*
+app.get("/", function (req, res) {
+  res.send("<head><link </head>Hello World");
+});
+*/
+
+app.listen(
+  3000,
+  () => console.log("server has started on http://localhost:3000 🚀"),
+);
+
+export { app };
