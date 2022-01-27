1b53ae556a54f98d450cce32ea10093f891a57c3
archi221 <Timon.Guenther@mail.de>
2022-01-27 17:07:14 +0100

    Bot

--- src/bot.ts
+++ src/bot.ts
@@ -22,6 +22,18 @@ export function createTelegramBot(token: string) {
             await bot.sendMessage({chat_id: message.message.chat.id, text: '[Deine Gruppe](https://www.latlmes.com/opinion/teenagers-spend-their-time-using-their-phones-is-it-worth-it-1)', parse_mode:'MarkdownV2' })
             return;
         }
+        if(text.startsWith("Newgroup")) {
+            let antwort = text.replace("NewGroup", "");
+            await bot.sendMessage({ chat_id: message.message.chat.id, text: `Neue Gruppe ${antwort} erstellt` })
+            await bot.sendMessage({chat_id: message.message.chat.id, text: '[Deine Gruppe](https://www.latlmes.com/opinion/teenagers-spend-their-time-using-their-phones-is-it-worth-it-1)', parse_mode:'MarkdownV2' })
+            return;
+        }
+        if(text.startsWith("newgroup")) {
+            let antwort = text.replace("NewGroup", "");
+            await bot.sendMessage({ chat_id: message.message.chat.id, text: `Neue Gruppe ${antwort} erstellt` })
+            await bot.sendMessage({chat_id: message.message.chat.id, text: '[Deine Gruppe](https://www.latlmes.com/opinion/teenagers-spend-their-time-using-their-phones-is-it-worth-it-1)', parse_mode:'MarkdownV2' })
+            return;
+        }
         if(text.startsWith("Help")) {
             await bot.sendMessage({ chat_id: message.message.chat.id, text: `Schreibe: \nNewGroup \nHelp` })
             return;
