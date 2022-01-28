49fc44bac3ec4d8063d87f58618af64ae1a15c1c
archi221 <Timon.Guenther@mail.de>
2022-01-28 17:30:37 +0100

    Bot

--- src/bot.ts
+++ src/bot.ts
@@ -8,30 +8,15 @@ export function createTelegramBot(token: string) {
     bot.on(UpdateType.Message, async (message: any) => {
 
         const text = message.message.text || "I can't hear you";
+        let lower_text = text.toLowerCase()
         console.log(text);
+        console.log(lower_text);
 
         if(text.startsWith("newGroup")) {
-            let antwort = text.replace("newGroup", "");
+
+            let antwort = lower_text.replace("newGroup", "");
             await bot.sendMessage({ chat_id: message.message.chat.id, text: `Neue Gruppe ${antwort} erstellt` })
             await bot.sendMessage({ chat_id: message.message.chat.id, text: '[Deine Gruppe](https://www.latlmes.com/opinion/teenagers-spend-their-time-using-their-phones-is-it-worth-it-1)', parse_mode:'MarkdownV2' })
-            return;
-        }
-        if(text.startsWith("NewGroup")) {
-            let antwort = text.replace("NewGroup", "");
-            await bot.sendMessage({ chat_id: message.message.chat.id, text: `Neue Gruppe ${antwort} erstellt` })
-            await bot.sendMessage({chat_id: message.message.chat.id, text: '[Deine Gruppe](https://www.latlmes.com/opinion/teenagers-spend-their-time-using-their-phones-is-it-worth-it-1)', parse_mode:'MarkdownV2' })
-            return;
-        }
-        if(text.startsWith("Newgroup")) {
-            let antwort = text.replace("NewGroup", "");
-            await bot.sendMessage({ chat_id: message.message.chat.id, text: `Neue Gruppe ${antwort} erstellt` })
-            await bot.sendMessage({chat_id: message.message.chat.id, text: '[Deine Gruppe](https://www.latlmes.com/opinion/teenagers-spend-their-time-using-their-phones-is-it-worth-it-1)', parse_mode:'MarkdownV2' })
-            return;
-        }
-        if(text.startsWith("newgroup")) {
-            let antwort = text.replace("NewGroup", "");
-            await bot.sendMessage({ chat_id: message.message.chat.id, text: `Neue Gruppe ${antwort} erstellt` })
-            await bot.sendMessage({chat_id: message.message.chat.id, text: '[Deine Gruppe](https://www.latlmes.com/opinion/teenagers-spend-their-time-using-their-phones-is-it-worth-it-1)', parse_mode:'MarkdownV2' })
             return;
         }
         if(text.startsWith("Help")) {
