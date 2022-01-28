dcd6558f6e7a2f6df980fccc093c05ffefa89202
archi221 <Timon.Guenther@mail.de>
2022-01-28 17:44:18 +0100

    app.ts angepasst

--- src/bot.ts
+++ src/bot.ts
@@ -7,14 +7,14 @@ export function createTelegramBot(token: string) {
 
     bot.on(UpdateType.Message, async (message: any) => {
 
-        const text = message.message.text || "I can't hear you";
-        let lower_text = text.toLowerCase()
-        console.log(text);
-        console.log(lower_text);
+        let text = message.message.text || "I can't hear you";
+//        let lower_text = text.toLowerCase()
+//        console.log(text);
+//        console.log(lower_text);
 
         if(text.startsWith("newGroup")) {
 
-            let antwort = lower_text.replace("newGroup", "");
+            let antwort = text.replace("newGroup", "");
             await bot.sendMessage({ chat_id: message.message.chat.id, text: `Neue Gruppe ${antwort} erstellt` })
             await bot.sendMessage({ chat_id: message.message.chat.id, text: '[Deine Gruppe](https://www.latlmes.com/opinion/teenagers-spend-their-time-using-their-phones-is-it-worth-it-1)', parse_mode:'MarkdownV2' })
             return;
