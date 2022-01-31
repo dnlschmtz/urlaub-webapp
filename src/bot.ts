c6e435cf36dba2a5596a4fae7a8243bc94262153
archi221 <Timon.Guenther@mail.de>
2022-01-31 12:27:23 +0100

    bot

--- src/bot.ts
+++ src/bot.ts
@@ -2,12 +2,15 @@ import { TelegramBot, UpdateType } from "https://deno.land/x/telegram_chatbot/mo
 import "https://deno.land/x/dot_env@0.2.0/load.ts"
 
 
+
 export function createTelegramBot(token: string) {
+}
     const bot = new TelegramBot(token);
 
     bot.on(UpdateType.Message, async (message: any) => {
 
         const text = message.message.text.toLowerCase() || "I can't hear you";
+
         console.log(text);
 
         if(text.startsWith("newgroup")) {
@@ -16,6 +19,14 @@ export function createTelegramBot(token: string) {
             await bot.sendMessage({ chat_id: message.message.chat.id, text: `Neue Gruppe ${antwort} erstellt` })
             await bot.sendMessage({ chat_id: message.message.chat.id, text: '[Klicke hier auf auf deine Gruppenseite zu kommen](https://www.latlmes.com/opinion/teenagers-spend-their-time-using-their-phones-is-it-worth-it-1)', parse_mode:'MarkdownV2' })
             return;
+
+        if(text.startsWith("newgroup")) {
+
+            let antwort = text.replace("info", "");
+            await bot.sendMessage({ chat_id: message.message.chat.id, text: `Deine Gruppen:
+            ` })
+            return;
+
         }
         if(text.startsWith("help")) {
 
@@ -28,6 +39,7 @@ export function createTelegramBot(token: string) {
         });
 
     bot.run({
+
         polling: true,
     });
 
