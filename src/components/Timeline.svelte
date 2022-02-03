0f6c37b688fd963c84bbe2ca59c1004353f4d446
BuildTools <dschmitz01@web.de>
2022-02-03 23:19:57 +0100

    Timeline aus Datenbank

--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,10 +1,14 @@
 <script>
     export function create(dates) {
-        let rows = [
-            [ 'Washington', new Date(1789, 3, 30), new Date(1797, 2, 4) ],
-            [ 'Adams',      new Date(1797, 2, 4),  new Date(1801, 2, 4) ],
-            [ 'Jefferson',  new Date(1801, 2, 4),  new Date(1809, 2, 4) ]
-        ];
+        let rows = [];
+
+        dates.forEach(element => {
+            rows.push([
+                element.name,
+                new Date(element.start),
+                new Date(element.end)
+            ]);
+        });
         
         google.charts.load('current', {'packages':['timeline']});
         google.charts.setOnLoadCallback(drawChart);
@@ -14,7 +18,7 @@
             let chart = new google.visualization.Timeline(container);
             let dataTable = new google.visualization.DataTable();
 
-            dataTable.addColumn({ type: 'string', id: 'Mitglied' });
+            dataTable.addColumn({ type: 'string', id: 'Name' });
             dataTable.addColumn({ type: 'date', id: 'Anfang' });
             dataTable.addColumn({ type: 'date', id: 'Ende' });
             dataTable.addRows(rows);
@@ -22,15 +26,6 @@
             let options = {
                 timeline: { singleColor: '#ffa947' }
             };
-
-            google.visualization.events.addListener(chart, 'ready', function () {
-                let rects = container.getElementsByTagName('rect');
-                Array.prototype.forEach.call(rects, function(rect) {
-                    if ((rect.getAttribute('x') === '0') && (rect.getAttribute('stroke') === 'none')) {
-                        rect.setAttribute('fill', '#eee');
-                    }
-                });
-            });
 
             chart.draw(dataTable, options);
         }
