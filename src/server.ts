1c61ae1c28fc39bad0218fd48c13d7c7aad1a9ad
BuildTools <dschmitz01@web.de>
2022-02-03 20:00:31 +0100

    Opine webserver aufgeräumt

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,24 @@ import { opine, serveStatic } from "https://deno.land/x/opine@2.1.1/mod.ts";
 import { dirname, join } from "https://deno.land/x/opine@2.1.1/deps.ts";
 
 
+const handleWebsocket = (socket: WebSocket) => {
+    socket.addEventListener("open", (_) => {
+        socket.send("ping");
+        console.log("sent ping to client");
+    });
+
+    socket.addEventListener("close", (_) => {
+        console.log("socket closed");
+    });
+
+    socket.addEventListener("message", (e) => {
+        if(e.data == "command") {
+            console.log("Received a message");
+        }
+    });
+}
+
+
 export function serveWeb(port: number) {
     const app = opine();
     const __dir = dirname(import.meta.url);
@@ -9,23 +27,6 @@ export function serveWeb(port: number) {
     // Serve static dirs
     app.use(serveStatic(join(__dir, "../public")));
     app.use(serveStatic(join(__dir, "../build")));
-
-    const handleWebsocket = (socket: WebSocket) => {
-        socket.addEventListener("open", (_) => {
-            socket.send("ping");
-            console.log("sent ping to client");
-        });
-    
-        socket.addEventListener("close", (_) => {
-            console.log("socket closed");
-        });
-    
-        socket.addEventListener("message", (e) => {
-            if(e.data == "command") {
-                console.log("Received a message");
-            }
-        });
-    }
 
     // Register websocket
     app.get("/ws", async (req, res, next) => {
@@ -37,16 +38,6 @@ export function serveWeb(port: number) {
         }
     
         next();
-    });
-    
-    app.use((_, res, __) => {
-        res.setHeader("access-control-allow-origin", "*");
-        res.setHeader(
-        "access-control-expose-headers",
-        "Upgrade,sec-websocket-accept,connection",
-        );
-    
-        res.send();
     });
 
     // 404 Error handling
