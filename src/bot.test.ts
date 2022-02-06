291d3070d9ae9f2c16517475d181eb8e2773a2c4
archi221 <Timon.Guenther@mail.de>
2022-02-06 03:47:37 +0100

    bot update

--- src/bot.test.ts
+++ src/bot.test.ts
@@ -1,13 +1,12 @@
-import { TelegramBot, UpdateType } from "https://deno.land/x/telegram_chatbot/mod.ts"
-import "https://deno.land/x/dot_env@0.2.0/load.ts"
+import { assertNotEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts"
 import { Bson } from "https://deno.land/x/mongo@v0.29.1/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
-import { assertNotEquals } from "https://deno.land/std@0.86.0/testing/asserts.ts"
-
 
 import { MongoDBConnector } from "./database/mongodb-connector.ts";
 
+import { TelegramBot, UpdateType } from "https://deno.land/x/telegram_chatbot/mod.ts"
 
+// Read MongoDB URL from .env file
 const MONGO_DB_URL = Deno.env.get("MONGO_DB_URL");
 if (!MONGO_DB_URL) throw new Error("MONGO_DB_URL is not provided");
 
@@ -16,16 +15,37 @@ const mongodb = new MongoDBConnector(MONGO_DB_URL);
 await mongodb.connect();
 
 let id: Bson.ObjectId | undefined;
-// Read MongoDB URL from .env file
-//const MONGO_DB_URL = Deno.env.get("MONGO_DB_URL");
-//if (!MONGO_DB_URL) throw new Error("MONGO_DB_URL is not provided");
+
+// Test for insert
+Deno.test("#1 Check for insert", async () => {
+    id = await mongodb.insert({
+        name: "Unit Test",
+        description: "Testbeschreibung",
+        targets: [
+            {
+                name: "Paris",
+                x: 0.0,
+                y: 0.0,
+                votes: 0,
+            }
+        ],
+        dates: []
+    });
+
+    assertNotEquals(id, undefined);
+});
+
+// Test for finding collection by id
+Deno.test("#2 Check finding collection by id", async () => {
+    if(id === undefined) return;
+
+    const foundCollection = await mongodb.find(id?.toString());
+    assertNotEquals(foundCollection, undefined);
+});
 
 
-export function createTelegramBot(token: string) {
 
-
-
-// Create Group
+//Create Group
 Deno.test("#1 Check Create Group", async () => {
 
     const bot = new TelegramBot(token);
@@ -53,29 +73,15 @@ Deno.test("#1 Check Create Group", async () => {
                 description: "",
                 targets: [],
                 dates: []
-                });
+            });
 
 
             await bot.sendMessage({ chat_id: message.message.chat.id, text: `Neue Gruppe ${antwort} erstellt` })
             await bot.sendMessage({ chat_id: message.message.chat.id, text: '[Klicke hier auf auf deine Gruppenseite zu kommen](https://localhost:3000/?id=${id})', parse_mode:'MarkdownV2' })
-            await bot.sendMessage({ chat_id: message.message.chat.id, text: `https://localhost:3000/?id=${id}` })
+            await bot.sendMessage({ chat_id: message.message.chat.id, text: "https://localhost:3000/?id=${id}" })
             console.log(id);
             return;
         }
-        if(text.startsWith("info")) {
-
-            await bot.sendMessage({ chat_id: message.message.chat.id, text: `Deine Gruppen:
-            ` })
-            return;
-
-        }
-        if(text.startsWith("help")) {
-
-            await bot.sendMessage({ chat_id: message.message.chat.id, text: `Schreibe: \nNewGroup \nHelp` })
-            return;
-        }
-
-        await bot.sendMessage({ chat_id: message.message.chat.id, text: `Unbekannter Befehl ${text}. Du kannst mit help eine liste der befehle bekommen.` })
 
         });
 
